99fa23ecd318143a61be662b1d619310b86d84f8
sony nurdianto <sonynurdianto445@gmail.com>
2024-07-09 20:05:27 +0700

    make background more soft

--- lua/sonynurdianto/plugins/colorscheme.lua
+++ lua/sonynurdianto/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
 		--vim.g.lualine_theme = "gruvbox_material"
 		vim.g.gruvbox_material_enable_italic = true
 		vim.g.gruvbox_material_enable_bold = true
+		vim.g.gruvbox_material_background = "soft"
 		vim.cmd("colorscheme gruvbox-material")
 	end,
 }
